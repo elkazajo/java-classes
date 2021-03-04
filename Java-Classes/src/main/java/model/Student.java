365155870f4e64e307029b3a1faec59c97032156
Marlen Zhumakov <mzhumakov@gmail.com>
2021-03-04 22:26:19 +0600

    delete setters for faculty, course and group

--- Java-Classes/src/main/java/model/Student.java
+++ Java-Classes/src/main/java/model/Student.java
@@ -89,24 +89,12 @@ public class Student implements Serializable {
         return faculty;
     }
 
-    public void setFaculty(String faculty) {
-        this.faculty = faculty;
-    }
-
     public String getCourse() {
         return course;
     }
 
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
     public String getGroup() {
         return group;
-    }
-
-    public void setGroup(String group) {
-        this.group = group;
     }
 
     @Override
